8ab8924fa2bf6264433fbded96a5efb277db2152
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2026-03-28 12:46:40 +0530

    feat: add company request models

--- internal/models/company.go
+++ internal/models/company.go
@@ -12,3 +12,17 @@ type Company struct {
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+type CreateCompanyRequest struct {
+	Name        string `json:"name"        validate:"required,min=2,max=100"`
+	Slug        string `json:"slug"        validate:"required,min=2,max=100,alphanum"`
+	Description string `json:"description" validate:"omitempty,max=1000"`
+	Website     string `json:"website"     validate:"omitempty,url"`
+}
+
+type UpdateCompanyRequest struct {
+	Name        *string `json:"name"        validate:"omitempty,min=2,max=100"`
+	Slug        *string `json:"slug"        validate:"omitempty,min=2,max=100,alphanum"`
+	Description *string `json:"description" validate:"omitempty,max=1000"`
+	Website     *string `json:"website"     validate:"omitempty,url"`
+}
